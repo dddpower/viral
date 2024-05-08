afa52684bcc5216798ec4ec7a6b75c156f073c2d
Hyoseok Jang <dddpower@gmail.com>
2024-05-08 17:46:40 +0900

    data frame generation finished

--- rerank.py
+++ rerank.py
@@ -17,7 +17,7 @@ G to T	0.000311342109361459
 T to A	7.57183086181891E-06
 T to C	5.12463411619656E-05
 T to G	6.88348260165355E-06
-	
+
 A to C	0.000480370596901429
 A to G	0.00161226521002784
 A to T	0.000924228464347168
@@ -38,34 +38,41 @@ import pandas as pd
 import argparse
 import json
 from enum import Enum
+
+
 class Virus(Enum):
     COV = 1
     FLU = 2
 
 
 def get_aa_from_position(codon_df, position):
-    return codon_df.iloc[position]['1AA']
+    return codon_df.iloc[position]["1AA"]
+
 
 def get_codon_from_position(codon_df, position):
-    return codon_df.iloc[position]['Codon']
+    return codon_df.iloc[position]["Codon"]
 
-               
 
 # receive amino_acid tranlate it to 3char codon
 def to_codons(ammino_acid):
     std_codon_table = CodonTable.unambiguous_dna_by_id[1]
-    codons = [codon for codon, aa in std_codon_table.forward_table.items() \
-        if aa == ammino_acid]
-    assert len(codons) > 0, f'ammino_acid = {ammino_acid}'
+    codons = [
+        codon
+        for codon, aa in std_codon_table.forward_table.items()
+        if aa == ammino_acid
+    ]
+    assert len(codons) > 0, f"ammino_acid = {ammino_acid}"
     # print(f'{ammino_acid} is mapped to {codons}')
     return codons
 
-    
+
 """
 Example
 prob of from wt(codon) to mutaion(ammino acid) 
 like AGG => 'C', AGG => "D", ....
 """
+
+
 def get_codon_aa_mutation_independent_probability(wt_codon, mut_aa, p_table):
     # 3 char for each codon
     # probability of mutation from wildtype based on p_table
@@ -75,16 +82,20 @@ def get_codon_aa_mutation_independent_probability(wt_codon, mut_aa, p_table):
             p *= p_table[ch1 + ch2]
         return p
 
-
     mut_codons = to_codons(mut_aa)
-    prob_list = [get_codon_independent_probability(wt_codon, mut_codon, p_table) for mut_codon in mut_codons]
-    assert len(prob_list) > 0, f"changed from {wt_codon}, to {mut_codons}, mut aa is {mut_aa}"
+    prob_list = [
+        get_codon_independent_probability(wt_codon, mut_codon, p_table)
+        for mut_codon in mut_codons
+    ]
+    assert (
+        len(prob_list) > 0
+    ), f"changed from {wt_codon}, to {mut_codons}, mut aa is {mut_aa}"
     return sum(prob_list)
 
 
 def wt_codon(position, wt_codon_df):
     try:
-        codon = wt_codon_df[wt_codon_df['pos'] == position]['Codon'].values[0]
+        codon = wt_codon_df[wt_codon_df["pos"] == position]["Codon"].values[0]
     except Exception as e:
         print(f"{e}. Error position is {position}")
     return codon
@@ -93,51 +104,62 @@ def wt_codon(position, wt_codon_df):
 def load_dict(file):
     return json.load(open(file))
 
+
 def process_viral_result_table(virus_type: Virus, weight_func):
-    h1n1_df = pd.read_csv("results/flu/semantics/analyze_semantics_flu_h1_bilstm_512.txt", delimiter='\t')
-    cov_df = pd.read_csv("results/cov/semantics/analyze_semantics_cov_bilstm_512.txt", delimiter='\t')
+    h1n1_df = pd.read_csv(
+        "results/flu/semantics/analyze_semantics_flu_h1_bilstm_512.txt", delimiter="\t"
+    )
+    cov_df = pd.read_csv(
+        "results/cov/semantics/analyze_semantics_cov_bilstm_512.txt", delimiter="\t"
+    )
     cov_wt_codon_df = pd.read_csv("COV_wildtype_codon.csv")
     h1n1_wt_codon_df = pd.read_csv("H1N1_wildtype_codon.csv")
 
-    rank_df, p_table, wt_codon_df = \
-        (cov_df, load_dict("SARS-COV2-static-probability.json"), cov_wt_codon_df) \
-            if virus_type == Virus.COV \
-            else (h1n1_df, load_dict("H1N1-static-probability.json"), h1n1_wt_codon_df)
-    
+    rank_df, p_table, wt_codon_df = (
+        (cov_df, load_dict("SARS-COV2-static-probability.json"), cov_wt_codon_df)
+        if virus_type == Virus.COV
+        else (h1n1_df, load_dict("H1N1-static-probability.json"), h1n1_wt_codon_df)
+    )
+
     # move position -1 to sync with original table index
-    wt_codon_df['pos'] -= 1
+    wt_codon_df["pos"] -= 1
     # get rid of junklike alphabet
-    rank_df = rank_df[~rank_df['wt'].str.contains('X|B|Z|J|U', case=False, na=False)]
-    rank_df = rank_df[~rank_df['mut'].str.contains('X|B|Z|J|U', case=False, na=False)]
-    
+    rank_df = rank_df[~rank_df["wt"].str.contains("X|B|Z|J|U", case=False, na=False)]
+    rank_df = rank_df[~rank_df["mut"].str.contains("X|B|Z|J|U", case=False, na=False)]
 
     # translate aa to codon
-    rank_df['codon'] = rank_df.apply(lambda row: wt_codon(row['pos'], wt_codon_df), axis=1)
+    rank_df["codon"] = rank_df.apply(
+        lambda row: wt_codon(row["pos"], wt_codon_df), axis=1
+    )
 
     # get wild type codon to mutation probability
-    rank_df['codon_prob'] = rank_df.apply(
-        lambda row: weight_func(wt_codon(row['pos'], wt_codon_df), 
-                                        row['mut'], p_table), axis=1)
+    rank_df["codon_prob"] = rank_df.apply(
+        lambda row: weight_func(wt_codon(row["pos"], wt_codon_df), row["mut"], p_table),
+        axis=1,
+    )
 
     # make rank columns
-    rank_df['grammar_rank'] = rank_df['prob'].rank(method='min', ascending=False)
-    rank_df['semantic_rank'] = rank_df['change'].rank(method='min', ascending=False)
-    rank_df['codon_mut_rank'] = rank_df['codon_prob'].rank(method='min', ascending=False)
-    rank_df['rank_sum'] = rank_df['grammar_rank'] + rank_df['semantic_rank'] + rank_df['codon_mut_rank']
-    rank_df['total_rank'] = rank_df['rank_sum'].rank(method='min', ascending=True)
+    rank_df["grammar_rank"] = rank_df["prob"].rank(method="min", ascending=False)
+    rank_df["semantic_rank"] = rank_df["change"].rank(method="min", ascending=False)
+    rank_df["codon_mut_rank"] = rank_df["codon_prob"].rank(
+        method="min", ascending=False
+    )
+    rank_df["rank_sum"] = (
+        rank_df["grammar_rank"] + rank_df["semantic_rank"] + rank_df["codon_mut_rank"]
+    )
+    rank_df["total_rank"] = rank_df["rank_sum"].rank(method="min", ascending=True)
     return rank_df
 
 
 # need to be careful of boundary exception
 def get_codon_aa_mutation_dependent_weight(codon_df, starting_position, mut_aa):
-
     """
-    Get probability-like weight value related to chance from wildtype codon to 
+    Get probability-like weight value related to chance from wildtype codon to
     some specific codon (mutation).
     Source contains prefix + wildtype + postfix (5 nucliotide chars)
     """
-    def get_dependent_weights(source, mut_codon, reduction_func) -> list:
 
+    def get_dependent_weights(source, mut_codon, reduction_func) -> list:
         """
         Current state contains from 0 to 4 position.
         Permutation number matches for
@@ -145,30 +167,42 @@ def get_codon_aa_mutation_dependent_weight(codon_df, starting_position, mut_aa):
         1 => [1:4], mut_codon[1]
         2 => [2:5], mut_codon[2].
         """
-        mut_dependent_prob_table = pd.read_excel('12276_2021_658_MOESM2_ESM.xlsx', sheet_name=3)
+        mut_dependent_prob_table = pd.read_excel(
+            "12276_2021_658_MOESM2_ESM.xlsx", sheet_name=3
+        )
+
         def calculate_weight(current, remain_permutation_number):
             # target_str format: "XX>XX"
             def get_mutation_dependent_probability(target_str):
                 if target_str[1] == target_str[3]:
                     return 1
-                sum = mut_dependent_prob_table['SARS-CoV-2'].sum()
-                val = mut_dependent_prob_table[mut_dependent_prob_table['Substition type'] == target_str]\
-                    ['SARS-CoV-2'].values[0]
+                sum = mut_dependent_prob_table["SARS-CoV-2"].sum()
+                val = mut_dependent_prob_table[
+                    mut_dependent_prob_table["Substition type"] == target_str
+                ]["SARS-CoV-2"].values[0]
                 return val / sum
 
             if not remain_permutation_number:
                 return 1
             head = remain_permutation_number[0]
-            keyword = current[head] + current[head + 1] + ">" + mut_codon[head] + current[head + 2]
+            keyword = (
+                current[head]
+                + current[head + 1]
+                + ">"
+                + mut_codon[head]
+                + current[head + 2]
+            )
             p = get_mutation_dependent_probability(keyword)
             current[head + 1] = mut_codon[head]
-            return p * calculate_weight(current, remain_permutation_number[head + 1:])
+            return p * calculate_weight(current, remain_permutation_number[head + 1 :])
 
-        return reduction_func(calculate_weight(source, numbers) for numbers in permutations([0, 1, 2], 3))
- 
+        return reduction_func(
+            calculate_weight(source, numbers) for numbers in permutations([0, 1, 2], 3)
+        )
 
-    assert get_aa_from_position(codon_df, starting_position) != mut_aa,\
-        f'starting position = {starting_position}, mut_aa = {mut_aa}'
+    assert (
+        get_aa_from_position(codon_df, starting_position) != mut_aa
+    ), f"starting position = {starting_position}, mut_aa = {mut_aa}"
     mut_codons = to_codons(mut_aa)
 
     pre = starting_position - 1
@@ -176,43 +210,46 @@ def get_codon_aa_mutation_dependent_weight(codon_df, starting_position, mut_aa):
     prefix = get_codon_from_position(codon_df, pre)[2]
     postfix = get_codon_from_position(codon_df, post)[0]
     wt_codon = get_codon_from_position(codon_df, starting_position)
-    probs = [sum(get_dependent_weights(prefix + wt_codon + postfix, mut_codon)) for mut_codon in mut_codons]
+    probs = [
+        sum(get_dependent_weights(prefix + wt_codon + postfix, mut_codon))
+        for mut_codon in mut_codons
+    ]
     return max(probs)
-    
+
 
 # cov_df = pd.read_csv("results/cov/semantics/analyze_semantics_cov_bilstm_512.txt", delimiter='\t')
-# 
+#
 # flu_df = pd.read_csv("results/flu/semantics/analyze_semantics_flu_h1_bilstm_512.txt", delimiter='\t')
 # rank_df = cov_df
 # # get rid of start and last position
 # max_pos = rank_df['pos'].max()
 # rank_df = rank_df[(rank_df['pos'] != 0) & (rank_df['pos'] != max_pos)]
-# 
+#
 # # get rid of junklike alphabet
 # rank_df = rank_df[~rank_df['wt'].str.contains('X|B|Z|J|U', case=False, na=False)]
 # rank_df = rank_df[~rank_df['mut'].str.contains('X|B|Z|J|U', case=False, na=False)]
-# 
-# 
-# 
+#
+#
+#
 # # translate aa to codon
 # rank_df['codon'] = rank_df.apply(lambda row: wt_codon(row['pos'], cov_wt_codon_df), axis=1)
 # # get wild type codon to mutation probability
 # rank_df['codon_prob'] = rank_df.apply(
 #     lambda row: aa_mutation_dependent_probability(cov_wt_codon_df, row['pos'], row['mut']), axis=1)
 # rank_df
-# 
-# 
-# 
+#
+#
+#
 # # make rank columns
 # rank_df['grammar_rank'] = rank_df['prob'].rank(method='min', ascending=False)
 # rank_df['semantic_rank'] = rank_df['change'].rank(method='min', ascending=False)
 # rank_df['codon_mut_rank'] = rank_df['codon_prob'].rank(method='min', ascending=False)
-# rank_df['rank_sum'] = rank_df['grammar_rank'] + rank_df['codon_mut_rank'] + rank_df['semantic_rank'] 
+# rank_df['rank_sum'] = rank_df['grammar_rank'] + rank_df['codon_mut_rank'] + rank_df['semantic_rank']
 # # rank_df['rank_sum'] = rank_df['codon_mut_rank']
 # rank_df['total_rank'] = rank_df['rank_sum'].rank(method='min', ascending=True)
 # rank_df
-# 
-# 
+#
+#
 # df2 = rank_df[rank_df['is_escape'] == True]
 # mean, std = df2['total_rank'].mean(), df2['total_rank'].std()
 # print(f'total number of cases are {len(df)}')
@@ -223,6 +260,6 @@ def get_codon_aa_mutation_dependent_weight(codon_df, starting_position, mut_aa):
 def get_codon_mutation_probability(Virus=None, indendent=True):
     return
 
-    
+
 if __name__ == "__main__":
     pass
--- test.py
+++ test.py
@@ -1,18 +1,23 @@
 from Bio.Data import CodonTable
 import pandas as pd
-from itertools import permutations, product
+from itertools import permutations, combinations
 import json
 import argparse
 from enum import Enum, auto
 
+all_amino_acids = "ARNDCEQGHILKMFPSTWYV"
+
+
 # receive amino_acid and translate it to 3char codon
 def to_codons(ammino_acid):
     std_codon_table = CodonTable.unambiguous_dna_by_id[1]
-    codons = [codon for codon, aa in std_codon_table.forward_table.items() \
-        if aa == ammino_acid]
-    assert len(codons) > 0, f'ammino_acid = {ammino_acid}'
+    codons = [
+        codon
+        for codon, aa in std_codon_table.forward_table.items()
+        if aa == ammino_acid
+    ]
+    assert len(codons) > 0, f"ammino_acid = {ammino_acid}"
     return codons
-
 
 
 # class Virus(Enum):
@@ -20,8 +25,6 @@ def to_codons(ammino_acid):
 #     FLU = auto()
 #     HIV = auto()
 
-if __name__ =="__main__":
-    print("need to input virus type when excuting the program")
 
 # Wildtype position table (Header: pos,Codon,1AA)
 wt_positions = pd.read_csv("cov_wildtype_codon.csv")
@@ -35,72 +38,137 @@ input_file_name = "12276_2021_658_MOESM2_ESM.xlsx"
 pos_dep_prob_table = pd.read_excel(input_file_name, sheet_name=3)
 
 def codon_from_aa_pos(source_position):
-    return wt_positions[wt_positions['pos'] == source_position]['Codon'].values[0]
+    return wt_positions[wt_positions["pos"] == source_position]["Codon"].values[0]
 
 
-"""Let's build_independent table"""
-def c2a_indep_prob(source_position, target:str):
+"""Build_independent table with funcions written below."""
+
+
+def pos_indep_prob(aa_pos, target: str):
     """wildtype codon to mutant amino_acid independent probability"""
-    source_codon = codon_from_aa_pos(source_position)
+    source_codon = codon_from_aa_pos(aa_pos)
     target_codons = to_codons(target)
     target_codons = filter(lambda x: x != source_codon, target_codons)
+
     def sum_iter():
         for target_codon in target_codons:
             p = 1
             for s, t in zip(source_codon, target_codon):
                 p *= pos_indep_prob_table[s + t]
             yield p
-        
+
     return sum(sum_iter())
 
 
-def test_c2a_indep_prob(position):
-    """test function for  c2a_indep_prob"""
-    all_amino_acids = "ARNDCEQGHILKMFPSTWYV"
-    return map(lambda x: c2a_indep_prob(position, x), (_ for _ in all_amino_acids))
+freq_sum = pos_dep_prob_table["SARS-CoV-2"].sum()
+def pos_dep_weight(source_position, mut_aa):
+    # must ignore first 'M'
+    def pos_dep_codon_weight(source_position, mut_codon):
+        """Compute the position-dependent mutation weight"""
+        def codon_weight(current: str, remain_permutation_number):
+            def single_prob(keyword):
+                val = pos_dep_prob_table[
+                    pos_dep_prob_table["Substition type"] == keyword
+                ]["SARS-CoV-2"].values[0]
+                return val / freq_sum
+
+            if not remain_permutation_number:
+                return 1
+
+            head = remain_permutation_number[0]
+            keyword = (
+                current[head - 1]
+                + current[head]
+                + ">"
+                + mut_codon[head - 1]
+                + current[head + 1]
+            )
+            p = single_prob(keyword)
+            # p = 1 if keyword[1] == keyword[3] else single_prob(keyword)
+            new_current = current[:head] + mut_codon[head - 1] + current[head + 1 :]
+            result = p * codon_weight(
+                new_current, remain_permutation_number[1:]
+            )
+            return 0 if result == 1 else result
+
+        pre = codon_from_aa_pos(source_position - 1)[2]
+        codon = codon_from_aa_pos(source_position)
+        end = codon_from_aa_pos(source_position + 1)[0]
+        # diff = map(lambda x: x[0] != x[1], zip(codon, mut_codon))
+        return sum(
+            codon_weight(pre + codon + end, _) for _ in permutations(_ for _ in range(1, 4) if codon[_ - 1] != mut_codon[_ - 1])
+        )
+
+    return sum(
+        map(lambda x: pos_dep_codon_weight(source_position, x), to_codons(mut_aa))
+    )
 
 
-# must ignore first 'M'
-def pos_dep_codon_weight(aa_source_position, mut_codon):
-    """Compute the position-dependent mutation weight"""
-    def codon_weight(current: str , remain_permutation_number):
-        def single_prob(keyword):
-            sum = pos_dep_prob_table["SARS-CoV-2"].sum()
-            val = pos_dep_prob_table[
-                pos_dep_prob_table["Substition type"] == keyword
-            ]["SARS-CoV-2"].values[0]
-            return val / sum
-
-        if not remain_permutation_number:
-            return 1
-
-        head = remain_permutation_number[0]
-        keyword = (
-                    current[head - 1]
-                    + current[head]
-                    + ">"
-                    + mut_codon[head - 1]
-                    + current[head + 1]
-                )
-        p = 1 if keyword[1] == keyword[3] else single_prob(keyword)
-        new_current = current[:head] + mut_codon[head - 1] + current[head + 1:]
-        result = p * codon_weight(new_current, remain_permutation_number[head + 1 :])
-        return 0 if result == 1 else result
-
-    pre = codon_from_aa_pos(aa_source_position - 1)[2]
-    codon = codon_from_aa_pos(aa_source_position)
-    end = codon_from_aa_pos(aa_source_position + 1)[0]
-    return sum(codon_weight(pre + codon + end, _) for _ in permutations(range(1, 4)))
-
-
+"""test functions"""
 
 
 # from 2 ~ until aa_pos
-def test_pos_dep_codon_weight(aa_pos):
-    def all_codons():
-        codon_table = CodonTable.unambiguous_dna_by_id[1]
-        return codon_table.forward_table.keys()
+def test_pos_dep_weight(aa_pos):
+    ...
+
+def test_pos_indep_prob(position):
+    """test function for  c2a_indep_prob"""
+    return map(lambda x: pos_indep_prob(position, x), (_ for _ in all_amino_acids))
 
 
-    for _ in range(2, aa_pos + 1):
-        yield list(map(lambda x: pos_dep_codon_weight(aa_pos, x), all_codons()))
+def make_extra_column():
+    """add extra rank columns to existing (cscs) table"""
+    cscs_path = "results/cov/semantics/analyze_semantics_cov_bilstm_512.txt"
+    cscs = pd.read_csv(cscs_path, sep="\t")
+    cscs["pos"] += 1
+
+    """remove unusable elements"""
+    # 1. get rid of row that matches condition "pos == 1"
+    cscs = cscs[cscs["pos"] != 1]
+
+    # 2. get rid of unusable alphabet
+    cscs = cscs[
+        ~cscs["wt"].str.contains("X|B|Z|J|U", case=False)
+        & ~cscs["mut"].str.contains("X|B|Z|J|U", case=False)
+    ]
+    cscs["pos_dep_weight"] = cscs.apply(
+        lambda row: pos_dep_weight(row["pos"], row["mut"]), axis=1
+    )
+    cscs["pos_indep_prob"] = cscs.apply(
+        lambda row: pos_indep_prob(row["pos"], row["mut"]), axis=1
+    )
+
+    # make rank columns
+    cscs["grammar rank"] = cscs["prob"].rank(method="min", ascending=False)
+    cscs["semantic rank"] = cscs["change"].rank(method="min", ascending=False)
+    cscs["pos_indep_rank"] = cscs["pos_indep_prob"].rank(method="min", ascending=False)
+    cscs["pos_dep_rank"] = cscs["pos_dep_weight"].rank(method="min", ascending=False)
+
+    base_list = ["grammar rank", "semantic rank"]
+    for x, y in combinations(base_list + ["pos_indep_rank"], 2):
+        cscs[x + "+" + y] = cscs[x] + cscs[y]
+    for x, y in combinations(base_list + ["pos_dep_rank"], 2):
+        cscs[x + "+" + y] = cscs[x] + cscs[y]
+
+    name1 = "grammar rank"
+    name2 = "semantic rank"
+    name3 = "pos_indep_rank"
+    name4 = "pos_dep_rank"
+    sum_rank0 = "+".join([name1, name2])
+    sum_rank1 = "+".join([name1, name2, name3])
+    sum_rank2 = "+".join([name1, name2, name4])
+    cscs[sum_rank0] = cscs[name1] + cscs[name2]
+    cscs[sum_rank1] = cscs[name1] + cscs[name2] + cscs[name3]
+    cscs[sum_rank2] = cscs[name1] + cscs[name2] + cscs[name4]
+    cscs[sum_rank0 + " rank"] = cscs[sum_rank0].rank(method="min", ascending=True)
+    cscs[sum_rank1 + " rank"] = cscs[sum_rank1].rank(method="min", ascending=True)
+    cscs[sum_rank2 + " rank"] = cscs[sum_rank2].rank(method="min", ascending=True)
+    # cscs["rank_sum"] = (
+    #     cscs["grammar_rank"] + cscs["semantic_rank"] + cscs["codon_mut_rank"]
+    # )
+    # cscs["total_rank"] = cscs["rank_sum"].rank(method="min", ascending=True)
+    cscs.to_csv("0507.csv", index=False)
+
+
+if __name__ == "__main__":
+    make_extra_column()
